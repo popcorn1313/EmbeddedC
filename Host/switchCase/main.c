509ae696508f8625136fb2b2e1d5944037f05034
popcorn1313 <110306035@g.nccu.edu.tw>
2025-05-09 11:43:12 +0800

    fix:error on switch case

--- Host/switchCase/main.c
+++ Host/switchCase/main.c
@@ -13,28 +13,62 @@ int main(void){
 
 	char type;
 	float area;
+	int a,b,c;
 	printf("Please enter the type of figures\n 't' for triangle\n 'z' for trapezoid\n"
-			"'c' for circle\n 's' for square\n 'r' for rectangle \n");
+			"'c' for circle\n 's' for square\n 'r' for rectangle \n"
+			"You have to enter the integer !");
 	scanf("%c", &type);
 
 	switch(type){
 	case 't':
-		int a,b;
 		printf("Please enter the base\n");
 		scanf("%d",&a);
 		printf("Please enter the height\n");
 		scanf("%d", &b);
 		area = a*b/2;
-		printf("Area is %f", area);
+		printf("Area is %0.2f\n", area);
+		wait();
+
 		break;
 	case 'z':
-		int c,d,e;
-		printf("Please enter ");
+		printf("Please enter the upper base\n");
+		scanf("%d", &a);
+		printf("Please enter the lower base\n");
+		scanf("%d", &b);
+		printf("Please enter the height\n");
+		scanf("%d", &c);
+		area = (a + b)*c/2;
+		printf("Area is %0.2f\n", area);
+		wait();
 
+		break;
+	case 'c':
+		printf("Please enter the radius\n");
+		scanf("%d", &a);
+		area = a*a*3.14;
+		printf("Area is %0.2f\n", area);
+		wait();
 
+		break;
+	case 's':
+		printf("Please enter the length\n");
+		area = a*a;
+		printf("Area is %0.2f\n", area);
+		wait();
+		break;
+	case 'r':
+		printf("Please enter the length\n");
+		scanf("%d", &a);
+		printf("Please enter the width\n");
+		scanf("%d", &b);
+		area = a * b;
+		printf("Area is %0.2f\n", area);
+		wait();
+		break;
+	default:
+		printf("Invlid input\n");
+		area = -1;
 	}
-
-
 
 
 
